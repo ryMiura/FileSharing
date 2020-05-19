1cc1dbea44e5b3749dab839980ca3e106d4d4795
ryMiura <63990420+ryMiura@users.noreply.github.com>
2020-05-19 10:05:41 +0900

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,16 +1,8 @@
 <?php
 
-//エラーメッセージ
-
-require_once("smarty/libs/Smarty.class.php");
-$smarty = new Smarty();
-
-$smarty->template_dir = './smarty/templates/';
-$smarty->compile_dir = './smarty/templates_c/';
-
+require_once("smarty_init.php");
+require_once("db_init.php");
 
 $smarty->display("signup.tpl");
 
-
-require_once("db_init.php");
  ?>
