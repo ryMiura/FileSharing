22455446eb3cc874a818e3d5a8abc9ddabb85ba1
ryMiura <63990420+ryMiura@users.noreply.github.com>
2020-05-20 15:15:41 +0900

    Update home.php

--- home.php
+++ home.php
@@ -1,12 +1,44 @@
 <?php
+//セッションスタート
 session_start();
-
+//データベース接続ファイル読み込み
 require_once("smarty_init.php");
+require_once("db_init.php");
 
-$e=1;
-
+//ログインされている状態だったら
 if(isset($_SESSION['us']) && $_SESSION['us'] != null){
- $smarty->assign("upload_disp",$e);
+ $smarty->assign("upload_disp",'OK');
+
+ try{
+
+$count=0;
+   $ps= $pdo->query("SELECT * FROM `table1`");
+
+$data=[];
+
+   if($ps->rowCount()>0){
+     while($r=$ps->fetch()){
+
+       $data[]=array(
+         $r['filename'],$r['date'],$r['size']
+       );
+
+
+     }
+
+     $smarty->assign('personaldata', $data);
+
+    
+   }
+
+
+ }catch(Exception $e){
+
+   echo "エラーが発生しました。";
+
+ }
+
+//ログインされていない状態だったら
 }else{
  $smarty->assign("upload_disp",'');
  session_destroy();
