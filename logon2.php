11b88c074ed8ce3ad55b018f18c74c44682949c9
ryMiura <63990420+ryMiura@users.noreply.github.com>
2020-05-20 09:03:37 +0900

    Update logon2.php

--- logon2.php
+++ logon2.php
@@ -1,65 +1,41 @@
 <?php
-
-
 //coockieにsessionid情報を入力
 session_start();
-
+//ファイル読み込み
+require_once("db_init.php");
+require_once("smarty_init.php");
+//新規登録ボタンがクリックされているときはsignup.phpへ遷移
 if(isset($_POST['signup'])){
     header('Location:signup.php');
 }
-
+//入力されたユーザー名、パスワード、アドレスを変数に代入
 $user = isset($_POST['u'])?htmlspecialchars($_POST['u'],ENT_QUOTES):null;
 $pass = isset($_POST['p'])?htmlspecialchars($_POST['p'],ENT_QUOTES):null;
+$email = isset($_POST['email'])?htmlspecialchars($_POST['email'],ENT_QUOTES):null;
 
-require_once("db_init.php");
+//ログインテーブルからIDパスワードをチェック
+$ps= $pdo->query("SELECT * FROM `logintable` WHERE `user` = '$user'");
+if($ps->rowCount()>0){
+  $r=$ps->fetch();
 
- ?>
-<!DOCTYPE html>
-<html lang="ja" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>トップ画面</title>
-  </head>
-  <body>
-<?php
-
-  
-  $ps= $db->query("SELECT pass FROM `logintable` WHERE `user` = '$user'");
-  if($ps->rowCount()>0){
-    $r=$ps->fetch();
-
-    if($r['pass'] === md5($pass)){
-      $_SESSION['us'] = $user;
-
-      //header('Location:home.php');
+  if(($r['pass'] === md5($pass)) && ($r['email'] == $email)){
+    $_SESSION['us'] = $user;
+    $smarty->assign("e",'');
+    //header('Location:home.php');
 ?>
+/
 <script>
   window.location.href = 'home.php';
 </script>
-
 <?php
-
-      }else{
+    }else{
     session_destroy();
-
-  ?>
-  <p>パスワードが違います<br>
-  <a href="logon.html">ログイン</a></p>
-  <?php
+    $smarty->assign("e",'入力されていない項目があります');
+    }
+  }else{
+    session_destroy();
+    $smarty->assign("e",'登録がみつかりません');
   }
-
-}else{
-  session_destroy();
-  ?>
-
-  <p>ユーザーが登録されていません<br>
-  <a href="logon.html">ログイン</a></p>
-  <?php
-}
-
-
+$smarty->display("logoncheck.tpl");
  ?>
 
-
-  </body>
-</html>
