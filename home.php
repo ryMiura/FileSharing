0693f747ec3dab5a1f8c10834d06bd79264449c8
ryMiura <63990420+ryMiura@users.noreply.github.com>
2020-05-21 10:26:43 +0900

    Update home.php

--- home.php
+++ home.php
@@ -4,6 +4,9 @@ session_start();
 //データベース接続ファイル読み込み
 require_once("smarty_init.php");
 require_once("db_init.php");
+require_once("file_download.php");
+require_once("Pager/Pager.php");
+
 
 //ログインされている状態だったら
 if(isset($_SESSION['us']) && $_SESSION['us'] != null){
@@ -17,14 +20,44 @@ if(isset($_SESSION['us']) && $_SESSION['us'] != null){
    if($ps->rowCount()>0){
      while($r=$ps->fetch()){
        $personaldata[]=array(
-         $r['filename'],$r['date'],$r['size']
+         $r['filename'],$r['date'],$r['size'],$r['id']
        );
      }
      $smarty->assign('personaldata', $personaldata);
    }
+   $options = array(
+     //トータルアイテム数
+     "totalItems" => 11,
+     //ページリンク数
+     "delta" => 5,
+     //1ページに表示させるアイテム数
+     "perPage" => 3
+   );
+
+   $pager =& Pager::factory($options);
+   $navi = $pager -> getLinks();
+   $smarty->assign('navi', $navi["all"]);
+
+
+   $currentPageID = $pager -> getCurrentPageID();
+   $index = ($currentPageID - 1) * 3 + 1;
+
+  print("<p>");
+  for($i = $index; $i < $index + 3 ; $i++){
+    if ($i <= 11){
+      print($i);
+      print(" ");
+    }
+  }
+  print("</p>");
+  print("現在のページ番号は".$currentPageID);
+
+
  }catch(Exception $e){
    echo "エラーが発生しました。";
  }
+
+
 //ログインされていない状態だったら
 }else{
  $smarty->assign("upload_disp",'');
