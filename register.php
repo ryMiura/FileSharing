bd6adf21918960355c8e85aa0035ea73ee18aaf6
ryMiura <63990420+ryMiura@users.noreply.github.com>
2020-05-22 11:12:45 +0900

    Update register.php

--- register.php
+++ register.php
@@ -15,8 +15,13 @@ $password=filter_input(INPUT_POST,'p');
 //正規表現
 if(!preg_match("/^[A-Za-z0-9]{4,16}$/",$password)){
   $err[]='パスワードは英数字で4文字以上、16文字以下ににしてください';
-
 }
+$email=filter_input(INPUT_POST,'email');
+
+if (!preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $email)) {
+    $err[]='アドレスの形式が不一致です';
+}
+
 //エラーがない時
 if(count($err) === 0){
 
@@ -26,10 +31,11 @@ if(count($err) === 0){
 
   try{
 
-    $db->query("INSERT INTO logintable (id, user, pass) VALUES (NULL, '$username', md5('$password'))");
+    $pdo->query("INSERT INTO logintable (id, user, pass,email) VALUES (NULL, '$username', md5('$password'),'$email')");
 
   }catch(Exception $e){
-
+  
+    $err[]='既に登録ずみのアドレスです';
 
   }
 
