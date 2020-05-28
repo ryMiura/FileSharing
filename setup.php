ae9ae1330520030284ab1c237efd34b9b6df4572
ryMiura <63990420+ryMiura@users.noreply.github.com>
2020-05-28 14:41:53 +0900

    Add files via upload

--- setup.php
+++ setup.php
@@ -1,76 +1,44 @@
 <?php
 session_start();
- ?>
-<!DOCTYPE html>
-<html lang="ja" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>アップロード完了</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class="container">
-
-      <div class="upload m-5">
-        <h3>アップロードしました</h3>
+require_once("db_init.php");
+require_once("smarty_init.php");
 
 
+if(isset($_SESSION['us']) && $_SESSION['us'] != null){
+  if(isset($_FILES['myf'])){
+    $smarty->assign("disp","OK");
+    //$_FILESに格納されている「myf」の情報を変数$fileに代入
+    $file = $_FILES['myf'];
 
-    <?php
-
-      if(isset($_SESSION['us']) && $_SESSION['us'] != null){
-
-        //$_FILESに格納されている「myf」の情報を変数$fileに代入
-        $file = $_FILES['myf'];
-        //アップロードされたデータを「filedata/ファイル名」に移動している
-        move_uploaded_file($file['tmp_name'],'./filedata/'.$file['name']);
-        print <<<eot
-        <p>ファイル名：{$file['name']}</p>
-        <p>サイズ：{$file['size']}バイト</p>
-        <p>MINEタイプ：{$file['type']}</p>
-        eot;
+    //アップロードされたデータを「filedata/ファイル名」に移動している
+    $pname = mb_convert_encoding($file['name'], "JIS", "auto");
+    move_uploaded_file($file['tmp_name'],'./filedata/'.$pname);
+    $smarty->assign("file",$file);
 
 
-
-
-        if(($file['type']=="image/jpeg")||($file['type']=="image/png")||($file['type']=="image/gif")){
-          print <<<eot
-          <img src = "filedata/{$file['name']}" width="300">
-          eot;
-        }
-        print <<<eot1
-        <p><button type="button" class="btn btn-warning mt-5 "onclick="location.href='home.php'">戻る</button></p>
-        eot1;
-        //データベースに追加
-        require_once("db_init.php");
-
-        try{
-
-          $a=$file['name'];
-          $b=date("Y/m/d");
-          $c=$file['size'];
-          $pdo->query("INSERT INTO `table1` VALUES (NULL, '$a', '1', '$b', 'user1','$c')");
-
-        }catch(Exception $e)
-        {
-          echo "登録できませんでした".$e->getmessage();
-
-        }
-
-
-    ?>
-    <?php
+    if(($file['type']=="image/jpeg")||($file['type']=="image/png")||($file['type']=="image/gif")){
+      $smarty->assign("img","OK");
     }else{
-      session_destroy();
-      print "<p>ログインして下さい</P>";
+      $smarty->assign("img","");
+    }
+    try{
+
+      $fname=$file['name'];
+      $fdate=date("Y/m/d");
+      $fsize=$file['size'];
+      $ope=$_SESSION['usid'];
+      $pdo->query("INSERT INTO `filetable` VALUES (NULL, '$fname', '$fsize', '$fdate', '$ope')");
+
+    }catch(Exception $e){
+      echo "登録できませんでした".$e->getmessage();
     }
 
-     ?>
-      </div>
 
-
-
-</div>
-
-  </body>
-</html>
+  }else{
+    $smarty->assign("disp","");
+  }
+  $smarty->display("setup.tpl");
+}else{
+  session_destroy();
+  header('Location:logon.html');
+}
